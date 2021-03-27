077e7b3be336db54235361cbb54dfb5e60d05784
Cedric Haase <cedrichaase@mailbox.org>
2021-03-27 15:35:48 +0100

    Use uri from env variable

--- database/database.go
+++ database/database.go
@@ -1,15 +1,15 @@
 package database
 
 import (
-    "context"
+	"context"
 	"log"
 	"time"
 
-    "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-    "ha.si/shorty/helper"
+	"ha.si/shorty/helper"
 )
 
 var client mongo.Client
@@ -18,55 +18,55 @@ var ctx context.Context
 var uri = helper.GetEnv("MONGODB_URI", "mongodb://localhost:27017")
 
 type ShortcutDocument struct {
-    Shortcut     string `json:"shortcut" bson:"shortcut" binding:"required"`
-    Url          string `json:"url" bson:"url" binding:"required"`
-    Created      time.Time `json:"created" bson:"created" binding:"required"`
-    LastAccessed time.Time `json:"lastAccessed" bson:"lastAccessed" binding:"required"`
+	Shortcut     string    `json:"shortcut" bson:"shortcut" binding:"required"`
+	Url          string    `json:"url" bson:"url" binding:"required"`
+	Created      time.Time `json:"created" bson:"created" binding:"required"`
+	LastAccessed time.Time `json:"lastAccessed" bson:"lastAccessed" binding:"required"`
 }
 
 func AddShortcut(shortcut string, url string) {
-    client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-    err = client.Connect(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var collection = client.Database("shorty").Collection("shortcuts")
-    collection.InsertOne(ctx, bson.D{
-        {"shortcut", shortcut},
-        {"url", url},
-        {"created", time.Now()},
-        {"lastAccessed", time.Now()},
-    })
+	var collection = client.Database("shorty").Collection("shortcuts")
+	collection.InsertOne(ctx, bson.D{
+		{"shortcut", shortcut},
+		{"url", url},
+		{"created", time.Now()},
+		{"lastAccessed", time.Now()},
+	})
 
-    defer client.Disconnect(ctx)
+	defer client.Disconnect(ctx)
 }
 
 func FindUrlByShortcut(shortcut string) string {
-    client, err := mongo.NewClient(options.Client().ApplyURI(uri))
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-    err = client.Connect(ctx)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    var collection = client.Database("shorty").Collection("shortcuts")
+	var collection = client.Database("shorty").Collection("shortcuts")
 
-    var result ShortcutDocument
-    err = collection.FindOne(ctx, bson.D{{"shortcut", shortcut}}).Decode(&result)
+	var result ShortcutDocument
+	err = collection.FindOne(ctx, bson.D{{"shortcut", shortcut}}).Decode(&result)
 
-    defer client.Disconnect(ctx)
+	defer client.Disconnect(ctx)
 
-    return result.Url
+	return result.Url
 }
